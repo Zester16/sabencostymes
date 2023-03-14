e8838269027f9987ec77591aeab18c50add5af8e
mharsh15 <mharsh15@gmail.com>
2023-03-14 23:01:23 +0530

    v_0_0_4 add image to frame

--- app/src/main/java/com/example/sabencostimes/MainActivity.kt
+++ app/src/main/java/com/example/sabencostimes/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Business")
 
                 }
             }
--- app/src/main/java/com/example/sabencostimes/view/NewsList.kt
+++ app/src/main/java/com/example/sabencostimes/view/NewsList.kt
@@ -1,16 +1,40 @@
 package com.example.sabencostimes.view
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.ImageLoader
+import coil.compose.AsyncImage
 import com.example.sabencostimes.domain.NYTNewsDataDomain
 
 @Composable
 fun NewsListColumn(newsList:List<NYTNewsDataDomain>){
     LazyColumn{
         items(items = newsList){
-               Text(text = (it.description.toString()))
+            Row(
+                horizontalArrangement =Arrangement.Center ,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+
+
+
+            ) {
+//                Column() {
+            AsyncImage(model = it.imageURL, contentDescription = it.description, modifier = Modifier.width(48.dp)
+            )
+                    Text(text = (it.title.toString()), fontSize = 20.sp, )
+
+                }
+
+
+            //}
             }
         }
 
