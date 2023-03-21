5efcf1813cc4a7c029ec33655c48fe99f8c7b627
mharsh15 <mharsh15@gmail.com>
2023-03-21 09:31:36 +0530

    v_0_0_7 Minor update on indie news composable

--- app/src/main/java/com/example/sabencostimes/view/NewsIndividualView.kt
+++ app/src/main/java/com/example/sabencostimes/view/NewsIndividualView.kt
@@ -1,14 +1,29 @@
 package com.example.sabencostimes.view
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import com.example.sabencostimes.domain.NYTNewsDataDomain
 import com.example.sabencostimes.parceable.NYTNewsParceable
 
 @Composable
 fun NewsIndividualView(navController:NavHostController,news:NYTNewsParceable) {
-    Text("This is Individual News")
+    Column() {
+        Text("This is Individual News")
+        news.let {
 
-    news?.title?.let { Text(it) }
-}
+            AsyncImage(model = it.imageURL, contentDescription = it.description)
+            Text(it.title.toString())
+            Text(it.description.toString())
+
+
+        }
+
+
+        }
+
+
+
+    }
