ecee1fc1d3e7de57c9b9bdb254339b086c41f279
mharsh15 <zesterschmid16@gmail.com>
2024-04-24 12:31:33 +0530

    v_0_0_46: add new webview for no js browsing, update the existing webview to link account with nyt and run in app itself

--- app/src/main/java/com/example/sabencostimes/webview/NewsWebView.kt
+++ app/src/main/java/com/example/sabencostimes/webview/NewsWebView.kt
@@ -1,13 +1,17 @@
 package com.example.sabencostimes.webview
 
+import android.os.Build
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
+//this web view takes into cognizant of saved account. if user has access to nytimes, user can access it
 fun NewsWebView( navController:NavHostController,newsUrl:String) {
     val trialUrl ="https://www.nytimes.com/series/asia-australia-morning-briefing"
     AndroidView(factory = {
@@ -17,6 +21,10 @@ fun NewsWebView( navController:NavHostController,newsUrl:String) {
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
             webViewClient = WebViewClient()
+            this.settings.safeBrowsingEnabled = true
+            this.getSettings().userAgentString="Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36"
+            this.canGoBackOrForward(3)
+            this.clearHistory()
             this.settings.javaScriptEnabled = true
             loadUrl(newsUrl)
         }
@@ -24,5 +32,21 @@ fun NewsWebView( navController:NavHostController,newsUrl:String) {
         it.loadUrl(newsUrl)
     })
 
+}
+@Composable
+fun NewsWebViewNoJs(navController: NavHostController,newsUrl: String){
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
 
+            loadUrl(newsUrl)
+            this.clearHistory()
+        }
+    }, update = {
+        it.loadUrl(newsUrl)
+    })
 }
